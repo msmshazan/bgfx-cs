dff4b0b1c5606d0b4518feb54312dfb691dfca8c
mohamed shazan <msmshazan1@gmail.com>
2020-01-01 19:58:36 +0530

    Fix for vcxproj property WindowsTargetPlatformVersion defaulting 8.1

--- buildbgfx.ps1
+++ buildbgfx.ps1
@@ -1,6 +1,14 @@
 .\setupshell.ps1
 cd bgfx 
-&devenv .build/projects/vs2019/bgfx.sln /Upgrade
+cd .build/projects/vs2019/
+$configFiles = Get-ChildItem . *.vcxproj -rec
+foreach ($file in $configFiles)
+{
+    (Get-Content $file.PSPath) |
+    Foreach-Object { $_ -replace "<WindowsTargetPlatformVersion>8.1</WindowsTargetPlatformVersion>", "<WindowsTargetPlatformVersion>10.0</WindowsTargetPlatformVersion>" } |
+    Set-Content $file.PSPath
+}
+cd ../../../
 &devenv .build/projects/vs2019/bgfx.sln /Build "Release|x64"
 &devenv .build/projects/vs2019/bgfx.sln /Build "Release|Win32"
 cd ..
