49b5d037b3db5a6cbd80b1242ad660e8df262804
mohamed shazan <msmshazan1@gmail.com>
2020-01-01 18:00:27 +0530

    Add ability to upgrade bgfx solution if required

--- buildbgfx.ps1
+++ buildbgfx.ps1
@@ -1,5 +1,6 @@
 .\setupshell.ps1
 cd bgfx 
+&devenv .build/projects/vs2019/bgfx.sln /Upgrade
 &devenv .build/projects/vs2019/bgfx.sln /Build "Release|x64"
 &devenv .build/projects/vs2019/bgfx.sln /Build "Release|Win32"
 cd ..
