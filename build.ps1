f4460656b9bec7ce377abfc399d80ad458e1ec87
Mohamed Shazan <msmshazan1@gmail.com>
2020-01-01 21:04:05 +0530

    Properly calling nuget

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 .\setupshell.ps1
 cd BGFX.Net
-nuget restore
+&nuget restore
 msbuild BGFX.Net.sln /P:'Configuration=Release'
 msbuild BGFX.Net.sln /P:'Configuration=Release;Platform=x64'
 msbuild BGFX.Net.sln /P:'Configuration=Release;Platform=x86'
