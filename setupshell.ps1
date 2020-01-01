904d09424bb8414e434a106c31edec6f1887adf3
mohamed shazan <msmshazan1@gmail.com>
2020-01-01 17:39:30 +0530

    Fix for azure-pipelines.yml for vspath

--- setupshell.ps1
+++ setupshell.ps1
@@ -1,3 +1,7 @@
 $PrevPath = Get-Location;
+if( $env:IS_PIPELINE -eq 'true' ){
+&Import-Module "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"; Enter-VsDevShell 11a2e51d  
+}else{
 &Import-Module "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"; Enter-VsDevShell 11a2e51d  
+}
 Set-Location $PrevPath
